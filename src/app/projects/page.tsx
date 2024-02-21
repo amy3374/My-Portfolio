b2f93522000b870cbc91e4ff97cf6b7f50738c17
amy3374 <119571479+amy3374@users.noreply.github.com>
2024-02-21 17:22:27 +0900

    node api 불러오기, data post

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,37 @@
+"use client";
 import React from "react";
+import { TOKEN, DATABASE_ID } from "../../../config";
 
-const Projects = () => {
+async function getPosts() {
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "Content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({ page_size: 100 }),
+  };
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+  const data = await res.json();
+  console.log("Data", data);
+  // const projectId = data.results.map((project: any) => project.id);
+  // console.log(`project id : ${projectId}`);
+  // return data as any[];
+
+  // return {
+  //   props: {},
+  // };
+}
+
+const Projects = async () => {
+  const posts = await getPosts();
+  console.log("posts", posts);
+
   return (
     <div>
       <h1>projects page</h1>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,9 +20,9 @@ const Footer = () => {
             <a className="text-gray-500">
               <svg
                 fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -32,9 +32,9 @@ const Footer = () => {
             <a className="ml-3 text-gray-500">
               <svg
                 fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -45,9 +45,9 @@ const Footer = () => {
               <svg
                 fill="none"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -59,9 +59,9 @@ const Footer = () => {
               <svg
                 fill="currentColor"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="0"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="0"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
